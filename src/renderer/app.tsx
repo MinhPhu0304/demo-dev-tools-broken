787b406a960b343a3a82b7b6d4feb4426389278e
minh <Lminhphu0304@gmail.com>
2020-04-22 15:38:25 +1200

    Remove test folder

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -6,9 +6,7 @@ import { AppContainer } from 'react-hot-loader';
 import Application from './components/Application';
 import store from './store';
 
-// Create main element
-const mainElement = document.createElement('div');
-document.body.appendChild(mainElement);
+const mainElement = document.getElementById('app');
 
 // Render components
 const render = (Component: () => JSX.Element) => {
